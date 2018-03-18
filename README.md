527b4db99d99166738f5178e866515ae91ea0f79
Leon Lee <leon.lee@berkeley.edu>
2018-03-18 10:56:08 -0700

    make unity scene full screen

--- README.md
+++ README.md
@@ -31,6 +31,12 @@
 
 
 #### Xcode and Unity Integration
+[Reference](https://github.com/jiulongw/swift-unity)
+- Copy 
+
+- Disable Metal API Validation. Go to Xcode->Product->Scheme->Edit Scheme, disable Metal API Validation. 
+
+
 
 
 ### Architecure
--- xcode/ARClay/ViewController.swift
+++ xcode/ARClay/ViewController.swift
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
             unityView.translatesAutoresizingMaskIntoConstraints = false
             let views = ["view": unityView]
             let w = NSLayoutConstraint.constraints(withVisualFormat: "|-0-[view]-0-|", options: [], metrics: nil, views: views)
-            let h = NSLayoutConstraint.constraints(withVisualFormat: "V:|-75-[view]-0-|", options: [], metrics: nil, views: views)
+            let h = NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[view]-0-|", options: [], metrics: nil, views: views)
             view.addConstraints(w + h)
         }
     }
